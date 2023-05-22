6e1484b8486db07331631aa6fad8fd45ea382703
Alex Colin <acolin3196@gmail.com>
2023-05-21 19:28:48 -0500

    js array list changes

--- assets/Java/script.js
+++ assets/Java/script.js
@@ -4,17 +4,26 @@ var h2El = document.getElementById("word");
 var winsEl = document.getElementById("wins")
 var lossesEl = document.getElementById("losses")
 var count = 30;
-var wordsList = ["javascript", "objects"];
+var wordsList = ["javascript", "objects", "arrays"];
 var randWordArr;
 var randWord;
 var _Arr = [];
 var wins = parseInt(localStorage.getItem("wins")) || 0;
 var losses = 0;
+var gameTimer;
 
 startBtn.addEventListener("click", function(e) {
+  startGame();
+});
+
+function startGame() {
   randomWord();
   startBtn.disabled = true;
-  var gameTimer = setInterval(function() {
+  count = 30;
+  timer.textContent = "timer: " + count;
+  lossesEl.textContent = losses;
+  winsEl.textContent = wins;
+  gameTimer = setInterval(function() {
     count--;
     timer.textContent = "timer: " + (count > 0 ? count : 0);
 
@@ -24,9 +33,10 @@ startBtn.addEventListener("click", function(e) {
       losses++;
       lossesEl.textContent = losses;
       localStorage.setItem("wins", wins);
+      startBtn.disabled = false;
     }
   }, 1000);
-});
+}
 
 function randomWord() {
   randWord = wordsList[Math.floor(Math.random() * wordsList.length)];
@@ -36,40 +46,116 @@ function randomWord() {
 }
 
 document.addEventListener("keyup", function(e) {
-    console.log(e.key, randWordArr);
-    for (let i = 0; i < randWordArr.length; i++) {
-      if (e.key === randWordArr[i]) {
-        _Arr[i] = e.key;
-      }
+  for (let i = 0; i < randWordArr.length; i++) {
+    if (e.key === randWordArr[i]) {
+      _Arr[i] = e.key;
     }
-    h2El.textContent = _Arr.join(" ");
-  
+  }
+  h2El.textContent = _Arr.join(" ");
+
+  if (_Arr.join("") === randWord) {
+    clearInterval(gameTimer);
+    h2El.textContent = "You Win!";
+    wins++;
+    winsEl.textContent = wins;
+    localStorage.setItem("wins", wins);
+    startBtn.disabled = false;
+  }
+});
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "Enter") {
+    e.preventDefault();
     if (_Arr.join("") === randWord) {
       clearInterval(gameTimer);
       h2El.textContent = "You Win!";
       wins++;
       winsEl.textContent = wins;
       localStorage.setItem("wins", wins);
+      startBtn.disabled = false;
+    } else {
+      losses++;
+      lossesEl.textContent = losses;
     }
-  });
+  }
+});
+
+
+
+// var startBtn = document.getElementById("start");
+// var timer = document.getElementById("timer");
+// var h2El = document.getElementById("word");
+// var winsEl = document.getElementById("wins")
+// var lossesEl = document.getElementById("losses")
+// var count = 30;
+// var wordsList = ["javascript", "objects"];
+// var randWordArr;
+// var randWord;
+// var _Arr = [];
+// var wins = parseInt(localStorage.getItem("wins")) || 0;
+// var losses = 0;
+// var gameTimer;
+
+// startBtn.addEventListener("click", function(e) {
+//   randomWord();
+//   startBtn.disabled = true;
+//   count--;
+//   timer.textContent = "timer: " + (count > 0 ? count : 0);
+//   var gameTimer = setInterval(function() {
+
+//     if (count <= 0) {
+//       clearInterval(gameTimer);
+//       h2El.textContent = "Game Over";
+//       losses++;
+//       lossesEl.textContent = losses;
+//       localStorage.setItem("wins", wins);
+//     }
+//   }, 1000);
+// });
+
+// function randomWord() {
+//   randWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+//   randWordArr = randWord.split("");
+//   _Arr = Array(randWordArr.length).fill("_");
+//   h2El.textContent = _Arr.join(" ");
+// }
+
+// document.addEventListener("keyup", function(e) {
+//     console.log(e.key, randWordArr);
+//     for (let i = 0; i < randWordArr.length; i++) {
+//       if (e.key === randWordArr[i]) {
+//         _Arr[i] = e.key;
+//       }
+//     }
+//     h2El.textContent = _Arr.join(" ");
   
-  document.addEventListener("keydown", function(e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      if (_Arr.join("") === randWord) {
-        clearInterval(gameTimer);
-        h2El.textContent = "You Win!";
-        wins++;
-        winsEl.textContent = wins;
-        localStorage.setItem("wins", wins);
-      } else {
-        losses++;
-      }
-    }
-  });
+//     if (_Arr.join("") === randWord) {
+//       clearInterval(gameTimer);
+//       h2El.textContent = "You Win!";
+//       wins++;
+//       winsEl.textContent = wins;
+//       localStorage.setItem("wins", wins);
+//     }
+//   });
   
-  winsEl.textContent = wins;
-  lossesEl.textContent = losses;
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "Enter") {
+//       e.preventDefault();
+//       if (_Arr.join("") === randWord) {
+//         clearInterval(gameTimer);
+//         h2El.textContent = "You Win!";
+//         wins++;
+//         winsEl.textContent = wins;
+//         localStorage.setItem("wins", wins);
+//       } else {
+//         losses++;
+//         lossesEl.textContent = losses;
+//       }
+//     }
+//   });
+  
+//   winsEl.textContent = wins;
+//   lossesEl.textContent = losses;
 
 
 
